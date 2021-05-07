d169d1ce5f5b008bd5e9670de5cda2b48e9b2a7e
Maria Belen Gatti <mbelen@hotmail.com>
2021-05-07 09:51:25 +0000

    First Kata Solved

--- vue-katas/src/router/index.js
+++ vue-katas/src/router/index.js
@@ -1,29 +1,44 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Kata1 from "@/views/Kata1.vue";
+import Kata2 from "@/views/Kata2.vue";
+import Kata3 from "@/views/Kata3.vue";
+import Kata4 from "@/views/Kata4.vue";
+import Kata5 from "@/views/Kata5.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/kata1",
+    name: "Kata 1",
+    component: Kata1,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/kata2",
+    name: "Kata 2",
+    component: Kata2,
+  },
+  {
+    path: "/kata3",
+    name: "Kata 3",
+    component: Kata3,
+  },
+  {
+    path: "/kata4",
+    name: "Kata 4",
+    component: Kata4,
+  },
+  {
+    path: "/kata5",
+    name: "Kata 5",
+    component: Kata5,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ vue-katas/src/views/Kata1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="(trivia, index) in trivias" :key="index">
+        <v-expansion-panel-header>
+          {{ trivia.question }}
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          {{ trivia.correct_answer }}
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import axios from "axios";
+
+export default Vue.extend({
+  data() {
+    return {
+      trivias: [],
+    };
+  },
+  async mounted() {
+    const response = await axios.get("https://opentdb.com/api.php?amount=10");
+    this.trivias = response.data.results;
+  },
+});
+</script>
+
+<style scoped></style>
